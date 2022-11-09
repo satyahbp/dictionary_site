a77361b2d4dc640b1ea9ab95d6c7d148ecb5e573
Satyajeet <satyajeet@localhost.localdomain>
2022-11-10 01:05:10 +0530

    completed UI functionalities

--- ui/script.js
+++ ui/script.js
@@ -1,21 +1,45 @@
 let words_dict = {
-    "dictionary": "a book to take words from",
-    "home": "the place where I find peace"
+    "dictionary": ["a book to take words from"],
+    "home": ["the place where I find peace", "a house"]
 }
 
 function capture_word(){
     
-    // capturing the word
     let input = document.getElementById("search").value;
-
-    // session storing the word
     sessionStorage.setItem("currentWord", input);
-
-    // changing the page
     window.open("word_page.html", "_self");
 }
 
 function present_word(){
+
     let the_word = sessionStorage.getItem("currentWord");
-    console.log(the_word);
+    let word_array = undefined;
+    let word_heading = document.getElementById("word_for_explanation");
+    let word_description = document.getElementById("word_description");
+    let word_list_point = document.getElementById("exp_ul");
+    let li = undefined;
+    let word_meaning = undefined
+    if (words_dict.hasOwnProperty(the_word)){
+
+        word_array = words_dict[the_word];
+        word_heading.innerHTML = the_word;
+
+        word_list_point.innerHTML = ""
+        word_array.forEach(element => {
+            li = document.createElement("li")
+            li.classList.add("desc_point");
+            word_meaning = document.createTextNode(element);
+            word_list_point.appendChild(li);
+            li.appendChild(word_meaning);
+        });
+    }
+    else{
+        word_heading.innerHTML = "The word '" + the_word +"' doesn't exist";
+        word_list_point.innerHTML = "";
+        not_exist_message = document.createElement("div");
+        not_exist_message.setAttribute("id", "not_exist_message");
+        word_description.appendChild(not_exist_message);
+        word_meaning = document.createTextNode("The word that you are searching for does not exist in our records. Sorry for the inconvenience.")
+        not_exist_message.appendChild(word_meaning);
+    }
 }
