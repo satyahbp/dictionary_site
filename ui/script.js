6a341ca659a6a81d909044aa4814e507430bab09
Satyajeet <satyajeet@localhost.localdomain>
2022-11-13 19:55:03 +0530

    completed the dictionary site

--- ui/script.js
+++ ui/script.js
@@ -1,5 +1,17 @@
 const xhttp = new XMLHttpRequest();
 
+const media_700_min_width = window.matchMedia("(min-width: 701px)");
+const media_700_601 = window.matchMedia("(max-width: 700px) and (min-width: 601px)");
+const media_600_551 = window.matchMedia("(max-width: 600px) and (min-width: 551px)");
+const media_550_501 = window.matchMedia("(max-width: 550px) and (min-width: 501px)");
+const media_500_451 = window.matchMedia("(max-width: 500px) and (min-width: 451px)");
+const media_450_401 = window.matchMedia("(max-width: 450px) and (min-width: 401px)");
+const media_400_351 = window.matchMedia("(max-width: 400px) and (min-width: 351px)");
+const media_350_301 = window.matchMedia("(max-width: 350px) and (min-width: 301px)");
+const media_300_251 = window.matchMedia("(max-width: 300px) and (min-width: 251px)");
+const media_251_max_width = window.matchMedia("(max-width: 251px)");
+
+
 function capture_word(){
     let input = document.getElementById("search").value;
     sessionStorage.setItem("currentWord", input);
@@ -28,11 +40,20 @@ function process_word(the_word, received_json){
     let word_list_point = document.getElementById("exp_ul");
     let li = undefined;
     let word_meaning = undefined;
+    let no_word = document.getElementById("no_word");
 
     if (received_json.hasOwnProperty("status") && (received_json["status"] == "success")){
 
         word_array = [received_json["meaning"]];
-        word_heading.innerHTML = the_word;
+        
+        word_heading.innerHTML = received_json["word"];
+
+        if (the_word.toLowerCase() != received_json["word"].toLowerCase()){
+            no_word.innerHTML = "There's no word as '" + the_word + "' with us. Do you mean to search for '" + received_json["word"] + "' ?";
+        }
+        else{
+            no_word.innerHTML = "";
+        }
 
         word_list_point.innerHTML = ""
         word_array.forEach(element => {
@@ -78,18 +99,202 @@ function continuous_word(){
 
 function show_word_recommendation(received_list){
     // console.log(received_list);
-    let recommendation_list = document.getElementById("recommendation_list");
-    recommendation_list.style.display = "block";
-    recommendation_list.innerHTML = "";
-    let list_element = undefined;
-    received_list.forEach(function(element, i) {
-        list_element = document.createElement("li");
-        list_element.classList.add("rec_li");
-        list_element.setAttribute("id", "rec_" + String(i));
-        list_element.innerHTML = element;
-        list_element.onclick = function(){capture_recommendation(this.id)};
-        recommendation_list.appendChild(list_element);
-    })
+    if (received_list.length > 0){
+        let recommendation_list = document.getElementById("recommendation_list");
+        recommendation_list.style.display = "block";
+        recommendation_list.innerHTML = "";
+        let list_element = undefined;
+        received_list.forEach(function(element, i) {
+            list_element = document.createElement("li");
+            list_element.classList.add("rec_li");
+            list_element.setAttribute("id", "rec_" + String(i));
+            list_element.innerHTML = element;
+            list_element.onclick = function(){capture_recommendation(this.id)};
+            recommendation_list.appendChild(list_element);
+        })
+
+        if (recommendation_list.classList.contains("word_page_rc")){
+            
+            if (media_700_min_width.matches){
+                if (received_list.length == 5){
+                    recommendation_list.style.top = "72px";
+                }
+                else if (received_list.length == 4){
+                    recommendation_list.style.top = "58px";
+                }
+                else if (received_list.length == 3){
+                    recommendation_list.style.top = "45px";
+                }
+                else if (received_list.length == 2){
+                    recommendation_list.style.top = "32px";
+                }
+                else if (received_list.length == 1){
+                    recommendation_list.style.top = "20px";
+                }
+            }
+            else if (media_700_601.matches){
+                if (received_list.length == 5){
+                    recommendation_list.style.top = "67px";
+                }
+                else if (received_list.length == 4){
+                    recommendation_list.style.top = "58px";
+                }
+                else if (received_list.length == 3){
+                    recommendation_list.style.top = "45px";
+                }
+                else if (received_list.length == 2){
+                    recommendation_list.style.top = "32px";
+                }
+                else if (received_list.length == 1){
+                    recommendation_list.style.top = "20px";
+                }
+            }
+            else if (media_600_551.matches){
+                if (received_list.length == 5){
+                    recommendation_list.style.top = "64px";
+                }
+                else if (received_list.length == 4){
+                    recommendation_list.style.top = "53px";
+                }
+                else if (received_list.length == 3){
+                    recommendation_list.style.top = "42px";
+                }
+                else if (received_list.length == 2){
+                    recommendation_list.style.top = "30px";
+                }
+                else if (received_list.length == 1){
+                    recommendation_list.style.top = "20px";
+                }
+            }
+            else if (media_550_501.matches){
+                if (received_list.length == 5){
+                    recommendation_list.style.top = "58px";
+                }
+                else if (received_list.length == 4){
+                    recommendation_list.style.top = "49px";
+                }
+                else if (received_list.length == 3){
+                    recommendation_list.style.top = "37px";
+                }
+                else if (received_list.length == 2){
+                    recommendation_list.style.top = "28px";
+                }
+                else if (received_list.length == 1){
+                    recommendation_list.style.top = "16px";
+                }
+            }
+            else if (media_500_451.matches){
+                if (received_list.length == 5){
+                    recommendation_list.style.top = "55px";
+                }
+                else if (received_list.length == 4){
+                    recommendation_list.style.top = "45px";
+                }
+                else if (received_list.length == 3){
+                    recommendation_list.style.top = "35px";
+                }
+                else if (received_list.length == 2){
+                    recommendation_list.style.top = "25px";
+                }
+                else if (received_list.length == 1){
+                    recommendation_list.style.top = "15px";
+                }
+            }
+            else if (media_450_401.matches){
+                if (received_list.length == 5){
+                    recommendation_list.style.top = "55px";
+                }
+                else if (received_list.length == 4){
+                    recommendation_list.style.top = "45px";
+                }
+                else if (received_list.length == 3){
+                    recommendation_list.style.top = "35px";
+                }
+                else if (received_list.length == 2){
+                    recommendation_list.style.top = "25px";
+                }
+                else if (received_list.length == 1){
+                    recommendation_list.style.top = "15px";
+                }
+            }
+            else if (media_400_351.matches){
+                if (received_list.length == 5){
+                    recommendation_list.style.top = "51px";
+                }
+                else if (received_list.length == 4){
+                    recommendation_list.style.top = "40px";
+                }
+                else if (received_list.length == 3){
+                    recommendation_list.style.top = "32px";
+                }
+                else if (received_list.length == 2){
+                    recommendation_list.style.top = "22px";
+                }
+                else if (received_list.length == 1){
+                    recommendation_list.style.top = "14px";
+                }
+            }
+            else if (media_350_301.matches){
+                if (received_list.length == 5){
+                    recommendation_list.style.top = "80px";
+                }
+                else if (received_list.length == 4){
+                    recommendation_list.style.top = "84px";
+                }
+                else if (received_list.length == 3){
+                    recommendation_list.style.top = "84px";
+                }
+                else if (received_list.length == 2){
+                    recommendation_list.style.top = "84px";
+                }
+                else if (received_list.length == 1){
+                    recommendation_list.style.top = "84px";
+                }
+            }
+            else if (media_300_251.matches){
+                if (received_list.length == 5){
+                    recommendation_list.style.top = "80px";
+                }
+                else if (received_list.length == 4){
+                    recommendation_list.style.top = "84px";
+                }
+                else if (received_list.length == 3){
+                    recommendation_list.style.top = "84px";
+                }
+                else if (received_list.length == 2){
+                    recommendation_list.style.top = "84px";
+                }
+                else if (received_list.length == 1){
+                    recommendation_list.style.top = "84px";
+                }
+            }
+            else if (media_251_max_width.matches){
+                if (received_list.length == 5){
+                    recommendation_list.style.top = "80px";
+                }
+                else if (received_list.length == 4){
+                    recommendation_list.style.top = "84px";
+                }
+                else if (received_list.length == 3){
+                    recommendation_list.style.top = "84px";
+                }
+                else if (received_list.length == 2){
+                    recommendation_list.style.top = "84px";
+                }
+                else if (received_list.length == 1){
+                    recommendation_list.style.top = "82px";
+                }
+            }
+        }
+        else if (recommendation_list.classList.contains("word_page_main")){
+
+        }
+    }
+    else{
+        recommendation_list.style.display = "none";
+    }
+    
+    
 }
 
 function capture_recommendation(id){
